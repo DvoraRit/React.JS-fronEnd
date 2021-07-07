6b6c73aa2c528ea402cceb32526b88ccf8d07b1a
Dvora Riterman <dvorasirota12@gmail.com>
2021-07-07 10:16:36 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,25 +9,39 @@ import Login from './component/logIn'
 import PrivateRoute from "./component/PrivateRoute"
 import ForgotPassword from './component/ForgotPassword'
 import UpdateProfile from './component/UpdateProfile'
+import {useRoutes} from 'hookrouter'
+import MembersComp from './component/Members'
+import MoviesComp from './component/movies'
+
+const routes ={
+  "/login" : () => <Login/>,
+  "/forgot-password" : () => <ForgotPassword/>,
+  "/SignUp" : () => <SignUp/>,
+  "/id*" : () => <Dashboard/>
+}
+
 
 function App() {
+  const match = useRoutes(routes)
+  /*  
+
+  */
   return (
     <Container
       className="d-flex align-items-center justify-content-center"
       style={{ minHeight: "100vh" }}>
         <div className="w-100" style={{height:"100vh"}} >
-          <Router>
-              <AuthProvider>
-                  <Switch>
-                      <Route path="/SignUp" component={SignUp} />
-                      <PrivateRoute exact path="/" component={Dashboard} />
-                      <Route path="/login" component={Login} />
-                      <Route path="/forgot-password" component={ForgotPassword} />
-                      <PrivateRoute path="/update-profile" component={UpdateProfile} />
-                  </Switch>
-              </AuthProvider>
-        </Router>
-      </div>
+
+                  <AuthProvider>
+                      <Switch>
+                         <Route exact path="/" component={Login} />
+                          <Route path="/forgot-password" component={ForgotPassword} />
+                          <Route path="/SignUp" component={SignUp} />
+                          <PrivateRoute  path="/Dashbord" component={Dashboard} />
+                      </Switch>
+                  </AuthProvider>
+               
+        </div>
     </Container>
     
   );
